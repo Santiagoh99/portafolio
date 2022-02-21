f567beefe704d32cf811f6e6478c10d060c68f67
santiherrera99@icloud.com <santiherrera99@icloud.com>
2022-02-21 10:32:55 -0300

    video

--- script.js
+++ script.js
@@ -31,3 +31,59 @@ function hadleSubmit(event) {
     $buttonmailto.setAttribute('href', `mailto:santiherrera@icloud.com?subject=${form.get('name')}//${form.get('mail')}//${form.get('phone')}&body=${form.get('mensaje')}`)
     $buttonmailto.click()
 }
+
+jQuery('iframe[src*="https://www.youtube.com/embed/"]').addClass("youtube-video");
+
+const btn = document.querySelector('.btn');
+
+const videoContainer = document.querySelector('.video-container');
+
+const close = document.querySelector('.close');
+
+btn.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    $('.youtube-video').each(function (index) {
+        $(this).attr('src', $(this).attr('src'));
+        return false;
+    });
+    videoContainer.classList.remove('show');
+})
+
+const btn2 = document.querySelector('#btn2');
+
+const videoContainer2 = document.querySelector('#video-container2');
+
+const close2 = document.querySelector('#close2');
+
+btn2.addEventListener('click', () => {
+    videoContainer2.classList.add('show');
+})
+
+close2.addEventListener('click', () => {
+    $('.youtube').each(function (index) {
+        $(this).attr('src', $(this).attr('src'));
+        return false;
+    });
+    videoContainer2.classList.remove('show');
+})
+
+const btn3 = document.querySelector('#btn3');
+
+const videoContainer3 = document.querySelector('#video-container3');
+
+const close3 = document.querySelector('#close3');
+
+btn3.addEventListener('click', () => {
+    videoContainer3.classList.add('show');
+})
+
+close3.addEventListener('click', () => {
+    $('.youtube2').each(function (index) {
+        $(this).attr('src', $(this).attr('src'));
+        return false;
+    });
+    videoContainer3.classList.remove('show');
+})
